fa9e16fa016a07c5537e31e2bcace0398b79bfdb
Carlos Garcia <chars24@gmail.com>
2017-12-09 15:05:54 -0600

    8. Creando EndPoint PUT/todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -50,6 +51,35 @@ app.get('/todos/:id', (req, res) => {
   }).catch((e) => {res.status(400).send()});
 });
 
+app.put('/todos/:id', (req, res) => {
+  let id = req.params.id
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  let body = _.pick(req.body, ['title', 'description', 'label', 'date', 'time', 'date', 'completed']);
+
+  Todo.findById(id).then((todo) => {
+    if(!todo) {
+      return res.status(404).send();
+    }
+
+    todo.title = body.title;
+    todo.description = body.description;
+    todo.label = body.label;
+    todo.date = body.date;
+    todo.time = body.time;
+    todo.completed = body.completed;
+
+    todo.save().then((doc) => {
+      res.send(doc);
+    }, (e) => {
+      res.status(400).send(e)
+    })
+  })
+
+});
 
 
 app.listen(port, () => {
