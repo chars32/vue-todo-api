f0145b7439d7ffb63aff5dc34285fe6dc4f10c8f
Carlos Garcia <chars24@gmail.com>
2017-12-08 21:43:34 -0600

    7. Seteando y haciendo Deploying a Heroku

--- server/db/moongose.js
+++ server/db/moongose.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
 // este hack para poder utilizar promesas.
 mongoose.Promise = global.Promise;
 
-// Conectamos a la bd con mongoose
-mongoose.connect('mongodb://localhost:27017/MyTodoApp', {
+// Conectamos a la bd con Heroku ó mongoose
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/MyTodoApp', {
   useMongoClient: true
 });
 
